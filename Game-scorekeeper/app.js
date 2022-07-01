b73b43b8a3a912b912739be286c169700c799afe
Aniruddha Bagal <bagalaniruddha@gmail.com>
2022-07-01 23:14:34 +0530

    Refactored the Code

--- Game-scorekeeper/app.js
+++ Game-scorekeeper/app.js
@@ -1,61 +1,60 @@
-const p1Btn = document.querySelector("#p1Btn");
-const p2Btn = document.querySelector("#p2Btn");
-const resetBtn = document.querySelector("#resetBtn");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Btn"),
+  disp: document.querySelector("#p1Disp"),
+}
 
-const p1Disp = document.querySelector("#p1Disp");
-const p2Disp = document.querySelector("#p2Disp");
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Btn"),
+  disp: document.querySelector("#p2Disp"),
+}
+
+const resetBtn = document.querySelector("#resetBtn");
 const maxScoreSelect = document.querySelector("#playto");
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 5;
 let isGameOver = false;
 
-p1Btn.addEventListener("click", function () {
+function updatesScores(player, opponent) {
   if (!isGameOver) {
-    p1Score += 1;
-    if (p1Score === winningScore) {
+    player.score += 1;
+    if (player.score === winningScore) {
       isGameOver = true;
-      p1Disp.classList.add('winner')
-      p2Disp.classList.add('loser')
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
+      player.disp.classList.add("winner");
+      opponent.disp.classList.add("loser");
+      player.button.disabled = true;
+      opponent.disabled = true;
     }
-    p1Disp.textContent = p1Score;
+    player.disp.textContent = player.score;
   }
+}
+
+
+p1.button.addEventListener("click", function () {
+
+  updatesScores(p1, p2);
 });
 
-p2Btn.addEventListener("click", function () {
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p2Disp.classList.add('winner')
-      p1Disp.classList.add('loser')
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
-    }
-    p2Disp.textContent = p2Score;
-  }
+p2.button.addEventListener("click", function () {
+
+  updatesScores(p2, p1);
 });
 
-resetBtn.addEventListener("click", reset);
 
 maxScoreSelect.addEventListener("change", function () {
   winningScore = parseInt(this.value);
   reset();
 });
 
+resetBtn.addEventListener("click", reset);
+
 function reset() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-
-  p1Disp.textContent = 0;
-  p2Disp.textContent = 0;
-
-  p1Disp.classList.remove('winner', 'loser')
-  p2Disp.classList.remove('winner', 'loser')
-  p1Btn.disabled = false;
-  p2Btn.disabled = false;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.disp.textContent = 0;
+    p.disp.classList.remove("winner", "loser");
+    p.button.disabled = false;
+  }
 }
