fb6cfaf12f8a1f04a36cb674bf1a99c47f9e3668
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-05-21 14:25:58 +0530

    Update script.js

--- 7.calculator/script.js
+++ 7.calculator/script.js
@@ -7,8 +7,9 @@ function display(val) {
 function solve() {
   let x = document.getElementById("result").value;
 
-  let y = eval(x)
-    (document.getElementById("result").value = y);
+  let y = eval(x);
+  
+  (document.getElementById("result").value = y);
 
   return y;
 }
